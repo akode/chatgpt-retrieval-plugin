611f004a15def4c7bfcc0728b7e44d350d66ac02
Andreas Kodewitz <andreas.kodewitz@gmail.com>
2023-04-09 11:00:37 +0200

    Vespa datastore implementation
    Signed-off-by: Andreas Kodewitz <andreas.kodewitz@gmail.com>

---
+++ datastore/providers/vespa_datastore.py
@@ -0,0 +1,251 @@
+import os
+from typing import Optional, Dict, List
+from datastore.datastore import DataStore
+from services.date import to_unix_timestamp
+from models.models import (
+    DocumentChunk,
+    QueryResult,
+    QueryWithEmbedding,
+    DocumentMetadata,
+    DocumentMetadataFilter,
+    DocumentChunkWithScore,
+)
+import vespa
+from vespa.application import Vespa
+from vespa.package import ApplicationPackage, Field, HNSW, RankProfile
+
+
+# Read environment variables for Vespa configuration
+VESPA_URL = os.environ.get("VESPA_URL", "http://localhost")
+VESPA_PORT = os.environ.get("VESPA_PORT", "19071")
+
+
+VECTOR_SIZE = 1536
+
+# Create the schema
+app_package = ApplicationPackage(name="documents")
+app_package.schema.add_fields(
+    Field(name="id", type="string", indexing=["attribute", "summary"]),
+    Field(
+        name="embedding",
+        type=f"tensor<float>(x[{VECTOR_SIZE}])",
+        indexing=["attribute", "summary", "index"],
+        ann=HNSW(
+            distance_metric="euclidean",
+            max_links_per_node=16,
+            neighbors_to_explore_at_insert=500,
+        ),
+    ),
+    Field(
+        name="text", type="string", indexing=["summary", "index"], index="enable-bm25"
+    ),
+    Field(name="document_id", type="string", indexing=["summary", "attribute"]),
+    Field(name="source_id", type="string", indexing=["summary", "index"]),
+    Field(name="source", type="string", indexing=["summary", "index"]),
+    Field(name="url", type="string", indexing=["summary", "index"]),
+    Field(name="created_at", type="long", indexing=["summary", "attribute"]),
+    Field(name="author", type="string", indexing=["summary", "index"]),
+)
+app_package.schema.add_rank_profile(
+    RankProfile(
+        name="vector-similarity",
+        inherits="default",
+        first_phase="closeness(embedding)",
+        inputs=[("query(query_embedding)", f"tensor<float>(x[{VECTOR_SIZE}])")],
+    )
+)
+
+
+class VespaDataStore(DataStore):
+    def __init__(
+        self,
+        vespa_client,
+    ):
+        self.client = vespa_client
+        self.tensor_fields = [
+            field.name
+            for field in self.client.application_package.schema.document.fields
+            if "tensor" in field.type
+        ]
+        self.schema = "documents"
+
+    def _chunk_to_flat_dict(self, chunk: DocumentChunk) -> Dict[str, any]:
+        chunk = chunk.dict()
+        metadata = chunk.pop("metadata")
+        chunk.update(metadata)
+        for field in self.tensor_fields:
+            chunk[field] = {"values": chunk[field]}
+
+        chunk["created_at"] = (
+            to_unix_timestamp(chunk["created_at"]) if chunk["created_at"] else None
+        )
+        return chunk
+
+    def _chunk_list_to_vespa_format(self, chunks: List[DocumentChunk]):
+        assert isinstance(
+            chunks[0], DocumentChunk
+        ), "List items should be DocumentChunk"
+        batch = [
+            {"id": chunk.id, "fields": self._chunk_to_flat_dict(chunk)}
+            for chunk in chunks
+        ]
+        return batch
+
+    @staticmethod
+    def _convert_filter(filter: DocumentMetadataFilter) -> Optional[str]:
+        """Converts DocumentMetadataFilter to Vespa YQL filter"""
+        filters = []
+        for field, value in filter.dict().items():
+            if value is not None:
+                if field == "start_date":
+                    filters.append(f"created_at >= {str(to_unix_timestamp(value))}")
+                elif field == "end_date":
+                    filters.append(f"created_at <= {str(to_unix_timestamp(value))}")
+                else:
+                    filters.append(f"{field} matches \"{str(value)}\"")
+        return " and ".join(filters)
+
+    def _query_to_vespa(self, query: QueryWithEmbedding) -> Dict[str, any]:
+        """Converts QueryWithEmbedding to a Vespa query
+
+        Args:
+            query: QueryWithEmbedding
+
+        Returns:
+            Vespa query (Dict)
+        """
+        yql = "select * from sources documents where ({targetHits:20}nearestNeighbor(embedding,query_embedding))"
+        if query.filter is not None:
+            filter_yql = self._convert_filter(query.filter)
+            yql = f"{yql} {filter_yql}"
+        return {
+            "yql": yql,
+            "input.query(query_embedding)": query.embedding,
+            "ranking.profile": "vector-similarity",
+            "hits": query.top_k,
+        }
+
+    def _vespa_result_to_document_chunk_with_score(self, hit) -> DocumentChunkWithScore:
+        """Converts a hit returned from Vespa to a DocumentChunkWithScore
+
+        Args:
+            hit: Hit returned from Vespa query
+
+        Returns:
+            DocumentChunkWithScore
+        """
+        fields = hit["fields"]
+        metadata = DocumentMetadata(**fields)
+        return DocumentChunkWithScore(
+            id=fields["id"],
+            text=fields["text"],
+            metadata=metadata,
+            embedding=fields["embedding"]["values"],
+            score=hit["relevance"],
+        )
+
+    @property
+    def _n_docs_ingested(self):
+        res = self.client.query(
+            body={
+                "yql": "select * from documents where true",
+                "type": "all",
+                "hits": 10,
+            }
+        )
+        return res.number_documents_indexed
+
+    async def _upsert(self, chunks: Dict[str, List[DocumentChunk]]) -> List[str]:
+        """Upsert chunks into the datastore.
+
+        Args:
+            chunks (Dict[str, List[DocumentChunk]]): A list of DocumentChunks to insert
+
+        Raises:
+            e: Error in upserting data.
+
+        Returns:
+            List[str]: The document_id's that were inserted.
+        """
+
+        feed_batch = [
+            item
+            for _, doc in chunks.items()
+            for item in self._chunk_list_to_vespa_format(doc)
+        ]
+        self.client.feed_batch(schema=self.schema, batch=feed_batch)
+        return list(chunks.keys())
+
+    async def _query(
+        self,
+        queries: List[QueryWithEmbedding],
+    ) -> List[QueryResult]:
+        """Query the QueryWithEmbedding against the MilvusDocumentSearch
+
+        Search the embedding and its filter in the collection.
+
+        Args:
+            queries (List[QueryWithEmbedding]): The list of searches to perform.
+
+        Returns:
+            List[QueryResult]: Results for each search.
+        """
+        requests = (self._query_to_vespa(query) for query in queries)
+
+        results = self.client.query_batch(requests)
+        print(results)
+        query_results = [
+            QueryResult(
+                query=query.query,
+                results=[
+                    self._vespa_result_to_document_chunk_with_score(item)
+                    for item in result.hits
+                ],
+            )
+            for query, result in zip(queries, results)
+        ]
+        print(query_results)
+
+        return query_results
+
+    async def delete(
+        self,
+        ids: Optional[List[str]] = None,
+        filter: Optional[DocumentMetadataFilter] = None,
+        delete_all: Optional[bool] = None,
+    ) -> bool:
+        """Delete the entities based either on the chunk_id of the vector,
+
+        Args:
+            ids (Optional[List[str]], optional): The document_ids to delete. Defaults to None.
+            filter (Optional[DocumentMetadataFilter], optional): The filter to delete by. Defaults to None.
+            delete_all (Optional[bool], optional): Whether to drop the collection and recreate it. Defaults to None.
+        """
+        if ids is None and filter is None and delete_all is None:
+            raise ValueError(
+                "Please provide one of the parameters: ids, filter or delete_all."
+            )
+
+        if delete_all == True:
+            raise NotImplementedError("Deleting all documents is not implemented yet.")
+
+        if filter is not None:
+            filter_condition = self._convert_filter(filter)
+            yql = f"select id, documentid from sources documents where {filter_condition}"
+            qbody = {
+                        "yql": yql,
+                        "hits": 10,
+                        "type": "all",
+                    }
+            res = self.client.query(body=qbody)
+            while len((res := self.client.query(body=qbody)).hits) > 0:
+                delete_resp = self.client.delete_batch([{"id": hit["fields"]["id"]} for hit in res.hits])
+                if not all(resp.status_code == 200 for resp in delete_resp):
+                    break
+            return True
+
+        if len(ids) > 0:
+            resp = self.client.delete_batch(
+                batch=[{"id": id} for id in ids], schema=self.schema
+            )
+            return resp.status_code == 200
---
+++ tests/datastore/providers/vespa/test_vespa_datastore.py
@@ -0,0 +1,161 @@
+
+import pytest
+from typing import List, Dict
+from models.models import (
+    DocumentChunkMetadata,
+    DocumentMetadataFilter,
+    DocumentChunk,
+    Query,
+    QueryWithEmbedding,
+    Source,
+)
+from vespa.application import Vespa
+from vespa.deployment import VespaDocker
+from datastore.providers.vespa_datastore import (
+    VespaDataStore, app_package, VECTOR_SIZE
+)
+
+def create_embedding(non_zero_pos: int, size: int) -> List[float]:
+    vector = [0.0] * size
+    vector[non_zero_pos % size] = 1.0
+    return vector
+
+@pytest.fixture
+def vespa_datastore() -> VespaDataStore:
+    vespa_docker = VespaDocker()
+    client = vespa_docker.deploy(app_package)
+    yield VespaDataStore(client)
+    vespa_docker.container.stop()
+    vespa_docker.container.remove()
+
+
+
+@pytest.fixture
+def initial_document_chunks() -> Dict[str, List[DocumentChunk]]:
+    first_doc_chunks = [
+        DocumentChunk(
+            id=f"first-doc-{i}",
+            text=f"Lorem ipsum {i}",
+            metadata=DocumentChunkMetadata(),
+            embedding=create_embedding(i, VECTOR_SIZE),
+        )
+        for i in range(4, 7)
+    ]
+    return {
+        "first-doc": first_doc_chunks,
+    }
+
+
+@pytest.fixture
+def document_chunks() -> Dict[str, List[DocumentChunk]]:
+    first_doc_chunks = [
+        DocumentChunk(
+            id=f"first-doc_{i}",
+            text=f"Lorem ipsum {i}",
+            metadata=DocumentChunkMetadata(
+                source=Source.email, created_at="2023-03-05", document_id="first-doc"
+            ),
+            embedding=create_embedding(i, VECTOR_SIZE),
+        )
+        for i in range(3)
+    ]
+    second_doc_chunks = [
+        DocumentChunk(
+            id=f"second-doc_{i}",
+            text=f"Dolor sit amet {i}",
+            metadata=DocumentChunkMetadata(
+                created_at="2023-03-04", document_id="second-doc"
+            ),
+            embedding=create_embedding(i + len(first_doc_chunks), VECTOR_SIZE),
+        )
+        for i in range(2)
+    ]
+    return {
+        "first-doc": first_doc_chunks,
+        "second-doc": second_doc_chunks,
+    }
+
+
+def test_chunk_to_flat_dict(vespa_datastore, initial_document_chunks):
+    flat_dict = vespa_datastore._chunk_to_flat_dict(initial_document_chunks["first-doc"][0])
+    assert set(flat_dict.keys()) == {"id", "text", "embedding", "author", "url", "document_id", "source", "source_id", "created_at"}
+
+
+def test_chunk_list_to_vespa_format(vespa_datastore, initial_document_chunks):
+    vespa_batch = vespa_datastore._chunk_list_to_vespa_format(initial_document_chunks["first-doc"])
+    assert len(vespa_batch) == 3
+    assert isinstance(vespa_batch[0], dict)
+    assert set(vespa_batch[0].keys()) == {"id", "fields"}
+
+
+def test_query_filter_date(vespa_datastore):
+    query = QueryWithEmbedding(
+        query="lorem",
+        top_k=1,
+        embedding=[0] * VECTOR_SIZE,
+        filter=DocumentMetadataFilter(
+            start_date="2000-01-03T16:39:57-08:00", end_date="2010-01-03T16:39:57-08:00"
+        ),
+    )
+
+    vespa_query = vespa_datastore._query_to_vespa(query)
+    assert "created_at >= 946946397" in vespa_query["yql"]
+
+
+@pytest.mark.asyncio
+async def test_upsert(vespa_datastore, initial_document_chunks):
+    document_ids = await vespa_datastore._upsert(initial_document_chunks)
+    chunks_ingested = vespa_datastore.client.query(body={"yql": "select * from documents where true", "type": "all", "hits": 10})
+    print(chunks_ingested)
+    assert len(document_ids) == 1
+    assert chunks_ingested.number_documents_indexed == 3
+
+
+@pytest.mark.asyncio
+async def test_query_returns_all_on_single_query(vespa_datastore, document_chunks):
+    # Fill the database with document chunks before running the actual test
+    await vespa_datastore._upsert(document_chunks)
+
+    query_embedding = [0.5, 0.5, 0.5, 0.5, 0.5] + [0.0]*(VECTOR_SIZE-5)
+    assert len(query_embedding) == VECTOR_SIZE
+
+    query = QueryWithEmbedding(
+        query="lorem",
+        top_k=5,
+        embedding=query_embedding,
+    )
+    query_results = await vespa_datastore._query(queries=[query])
+
+    assert 1 == len(query_results)
+    assert "lorem" == query_results[0].query
+    assert 5 == len(query_results[0].results)
+
+
+@pytest.mark.asyncio
+async def test_delete_removes_by_document_id_filter(
+    vespa_datastore,
+    document_chunks,
+):
+    # Fill the database with document chunks before running the actual test
+    await vespa_datastore._upsert(document_chunks)
+
+    assert vespa_datastore._n_docs_ingested == 5, "There should be 5 chunks in store after upsert; found {vespa_datastore._n_docs_ingested}"
+
+    await vespa_datastore.delete(
+        filter=DocumentMetadataFilter(document_id="first-doc")
+    )
+
+    assert vespa_datastore._n_docs_ingested == 2, "There should be 2 chunks in store after delete; found {vespa_datastore._n_docs_ingested}"
+
+@pytest.mark.skip()
+@pytest.mark.asyncio
+async def test_delete_removes_all(
+    vespa_datastore,
+    document_chunks,
+):
+    # Fill the database with document chunks before running the actual test
+    await vespa_datastore._upsert(document_chunks)
+
+    await vespa_datastore.delete(delete_all=True)
+
+    assert 0 == vespa_datastore._n_docs_ingested
